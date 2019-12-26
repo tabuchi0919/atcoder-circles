628a2f11252e08cf1f669051dc2cad60d9da9c8c
tabuchi-ga-tech <k_tabuchi@ga-tech.co.jp>
2019-12-26 21:04:48 +0900

    virtualコンテストの種類を追加

--- app/models/virtual_contest.rb
+++ app/models/virtual_contest.rb
@@ -1,8 +1,23 @@
 class VirtualContest < ApplicationRecord
-  VALID_URL_REGEX = /\Ahttps:\/\/not-522.appspot\.com\/contest\/\d+\z/i
-
   validates :title, presence: true
-  validates :url, presence: true, format: { with: VALID_URL_REGEX }
-
+  validates :url, presence: true
+  validate :valid_url?
   belongs_to :circle
+
+  def valid_url?
+    host = URI.parse(url).host
+    return if [
+      'kenkoooo.com',
+      'not-522.appspot.com',
+      'onlinejudge.u-aizu.ac.jp',
+      'codeforces.com'
+    ].include?(host)
+
+    errors.add(url: :invalid)
+  rescue URI::InvalidURIError
+    errors.add(url: :invalid)
+  end
+
 end
+
+
