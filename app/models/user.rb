048ca880bc078948fc4ca041955797b72616eab0
tabuchi0919 <tabuchi.igo.0919@gmail.com>
2020-06-01 11:53:36 +0900

    正規表現修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :user_name, presence: true, uniqueness: true, format: {with: /\A[a-zA-z]\w{2,15}\z/}
+  validates :user_name, presence: true, uniqueness: true, format: {with: /\A\w+\z/}
 
   has_many :circle_users
   has_many :circles, through: :circle_users
